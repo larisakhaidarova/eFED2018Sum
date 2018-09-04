b1b661d7db86d5e40bf21c4d08b546f542011e4a
Larisa Khaidarova <36931280+lkhaidarova@users.noreply.github.com>
2018-09-04 23:12:58 +0400

    working tabs on index.html

--- js/tabs.js
+++ js/tabs.js
@@ -1,60 +1,31 @@
-var temperatureTab = document.getElementsByClassName('temperature-graph');
-var rainfallExpectancyTab = document.getElementsByClassName('rainfall-expectancy');
-var windTab = document.getElementsByClassName('wind-and-time');
+var tabs = document.getElementsByClassName('inset');
+var button = document.getElementsByClassName('detailedinfo');
+var buttonsNavigation = document.getElementById('buttons-navigation');
 
-var temperatureClick = document.getElementsByClassName('temp-nav');
-temperatureClick[0].addEventListener('click', temperatureTransition);
+buttonsNavigation.addEventListener('click', function(event){
+    if(event.target.classList.contains('detailedinfo')){
+      index = parseInt(event.target.dataset.weatherbutton);
+      changeTabs(index);
+    }
+})
 
-var precipitationClick = document.getElementsByClassName('precipitation-nav');
-precipitationClick[0].addEventListener('click', rainfallExpectancyTransition);
-
-var windClick = document.getElementsByClassName('wind-nav');
-windClick[0].addEventListener('click', windTransition);
-
-function temperatureTransition(event) {
-    if (temperatureTab[0].classList.contains("active-tab")) {
-        temperatureTab[0].classList.remove("active-tab");
-        temperatureClick[0].classList.contains("index-page-nav-active");
-        temperatureClick[0].classList.remove("index-page-nav-active");
-    }
-	if (event.target.className === 'temp-nav') {
-      rainfallExpectancyTab[0].classList.remove("active-tab");
-      windTab[0].classList.remove("active-tab");
-      temperatureTab[0].classList.add("active-tab");
-      precipitationClick[0].classList.remove("index-page-nav-active");
-      windClick[0].classList.remove("index-page-nav-active");
-      temperatureClick[0].classList.add("index-page-nav-active");
-    }
-}
-
-function rainfallExpectancyTransition(event) {
-    if (rainfallExpectancyTab[0].classList.contains("active-tab")) {
-        rainfallExpectancyTab[0].classList.remove("active-tab");
-        precipitationClick[0].classList.contains("index-page-nav-active");
-        precipitationClick[0].classList.remove("index-page-nav-active");
-    }
-    if (event.target.className === 'precipitation-nav') {
-       windTab[0].classList.remove("active-tab");
-       temperatureTab[0].classList.remove("active-tab");
-       rainfallExpectancyTab[0].classList.add("active-tab");
-       windClick[0].classList.remove("index-page-nav-active");
-       temperatureClick[0].classList.remove("index-page-nav-active");
-       precipitationClick[0].classList.add("index-page-nav-active");
-    
-    }
-}
-function windTransition(event) {
-    if (windTab[0].classList.contains("active-tab")) {
-        windTab[0].classList.remove("active-tab");
-        windClick[0].classList.contains("index-page-nav-active");
-        windClick[0].classList.remove("index-page-nav-active");
-    }
-    if (event.target.className === 'wind-nav') {
-        temperatureTab[0].classList.remove("active-tab");
-        rainfallExpectancyTab[0].classList.remove("active-tab");
-        windTab[0].classList.add("active-tab");
-        temperatureClick[0].classList.remove("index-page-nav-active");
-        precipitationClick[0].classList.remove("index-page-nav-active");
-        windClick[0].classList.add("index-page-nav-active");
-    }
+function changeTabs() {
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove('active-tab');
+  }
+  for (var i = 0; i < button.length; i++) {
+    button[i].classList.remove('active2');
+  }
+  if(index == 0){
+    tabs[0].classList.add('active-tab');
+    button[0].classList.add('active2');
+  }
+  else if(index == 1){
+    tabs[1].classList.add('active-tab');
+    button[1].classList.add('active2');
+  }
+  else if(index == 2){
+    tabs[2].classList.add('active-tab');
+    button[2].classList.add('active2');
+  }
 }
